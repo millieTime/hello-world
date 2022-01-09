440805104c761672488e172abf355cdefa950aaa
millieTime <thisismynewemailsoyeah@gmail.com>
2022-01-08 22:07:30 -0700

    added youtube video link

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,5 @@
 from functools import reduce
 ascii = [72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100]
-letters = [*map(lambda x: chr(x), ascii)]
+letters = map(lambda x: chr(x), ascii)
 phrase = reduce(lambda a, b: a+b, letters)
 print(phrase)
